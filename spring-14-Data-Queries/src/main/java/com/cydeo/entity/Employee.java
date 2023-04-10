63ae538d8f92cdd95db600c1efe59c47428804e9
Malik_Git <malikganiev2002@gmail.com>
2023-04-10 15:53:51 -0400

    Spring Data Queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@Data
+public class Employee extends BaseEntity {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private Integer salary;
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+    @ManyToOne
+    @JoinColumn(name="department")
+    private Department department;
+}
